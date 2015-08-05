92f50497e24b7b90e888d93efd88359608d65f40
Alex <alexpetit12@hotmail.com>
2015-08-04 21:41:35 -0400

    Added a few methods to list classes

---
+++ listImplementations/list_implementation.py
@@ -0,0 +1,166 @@
+class Node:
+    """
+    Basic implementation of a node class
+    """
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+        
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setData(self, newdata):
+        self.data = newdata
+        
+    def setNext(self, newnext):
+        self.next = newnext
+        
+class UnorderedList:
+    """
+    Basic implementation of an unordered list
+    """
+    def __init__(self):
+        self.head = None
+    
+    def isEmpty(self):
+        return self.head == None
+    
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+        
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+            
+        return count
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+                
+        return found
+    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else: 
+                previous = current
+                current = current.getNext()
+                
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+    
+    def index(self, item):
+        i = 0
+        current = self.head
+        while current != None and current.getData() != item:
+            current = current.getNext()
+            i += 1
+                
+        return i
+            
+class OrderedList:
+    """
+    Basic implementation of an ordered list
+    """
+    def __init__(self):
+        self.head = None
+        
+    def isEmpty(self):
+        return self.head == None
+    
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+                
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+        
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+            
+        return count
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                return True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+                    
+        return found
+                    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while current != None and not found:
+            if current == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+            
+    def index(self, item):
+        i = 0
+        current = self.head
+        while current != None and current.getData() != item:
+            current = current.getNext()
+            i += 1
+                
+        return i
+            
+# test
+ordered_list = OrderedList()
+ordered_list.add(5)
+ordered_list.add(10)
+ordered_list.add(2)
+ordered_list.add(1)
+print(ordered_list.size())
+print(ordered_list.index(5))
